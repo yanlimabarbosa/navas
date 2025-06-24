6241e35d202fef976efed6d1e305327b05035b3e
Yan Lima Barbosa <yanbr763@gmail.com>
2025-06-24 20:47:05 -0300

    fix: update image path in excelProcessor to use the correct image filename from the Excel data

--- navas-front/src/utils/excelProcessor.ts
+++ navas-front/src/utils/excelProcessor.ts
@@ -55,7 +55,7 @@ export const processExcelFile = (file: File): Promise<ProductGroup[]> => {
             specifications: row.Diferencial,
           }));
 
-          let imgPath = `imagens_produtos/${firstRow.Codigo}.png`;
+          let imgPath = `imagens_produtos/${firstRow.Imagem}.png`;
           if (imgPath.startsWith('/')) imgPath = imgPath.slice(1);
           return {
             id: `group-${position}`,
